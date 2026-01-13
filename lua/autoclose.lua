48b8b178c96c377ef91aed164ee3b66a74a85293
Brandon Young <brandon@byitkc.com>
2026-01-13 15:07:06 -0600

    testing the auto-close exception when the next character is not whitespace/linebreaks

--- lua/autoclose.lua
+++ lua/autoclose.lua
@@ -15,13 +15,15 @@ local default_pairs = {
 M.config = {
   pairs = default_pairs,
   enabled = true,
+  check_whitespace = true,  -- Only auto-close if next char is whitespace/blank
 }
 
 -- Setup function to configure the plugin
-function M.setup(opts)
+unction M.setup(opts)
   opts = opts or {}
   M.config.pairs = opts.pairs or default_pairs
   M.config.enabled = opts.enabled ~= false
+  M.config.check_whitespace = opts.check_whitespace ~= false  -- Default true
 
   -- Set up the keymaps for each opening character
   M.create_mappings()
@@ -36,25 +38,42 @@ function M.create_mappings()
   end
 end
 
+-- Check if character is whitespace
+local function is_whitespace(char)
+  return char == ' ' or char == '\t' or char == '\n' or char == '\r'
+end
+
 -- Main auto-close function
 function M.autoclose(open_char, close_char)
   if not M.config.enabled then
     return open_char
   end
 
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local next_char = col < #line and line:sub(col + 1, col + 1) or ''
+
   -- For symmetric pairs (like quotes), check if we should skip closing
   if open_char == close_char then
-    local line = vim.api.nvim_get_current_line()
-    local col = vim.api.nvim_win_get_cursor(0)[2]
-
     -- If next char is the closing char, skip it instead of inserting
-    if col < #line and line:sub(col + 1, col + 1) == close_char then
+    if next_char == close_char then
       return '<Right>'
     end
   end
 
- -- Insert both characters and move cursor between them
-  return open_char .. close_char .. '<Left>'
+  -- If whitespace checking is enabled, only auto-close in appropriate contexts
+  if M.config.check_whitespace then
+    -- Only auto-close if next character is whitespace or end of line
+    if next_char == '' or is_whitespace(next_char) then
+      return open_char .. close_char .. '<Left>'
+    end
+
+    -- Don't auto-close, just insert the opening character
+    return open_char
+  else
+    -- Always auto-close if whitespace checking is disabled
+    return open_char .. close_char .. '<Left>'
+  end
 end
 
 -- Toggle the plugin on/off
